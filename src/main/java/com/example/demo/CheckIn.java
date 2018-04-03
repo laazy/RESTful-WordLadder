7ca51ec2967be7ba2386a38fd1d22bf2f7921775
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2018-04-03 21:26:05 +0800

    initial commit

---
+++ src/main/java/com/example/demo/CheckIn.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Vector;
+
+@RestController
+public class CheckIn {
+    private Vector<String> Ladder;
+
+}
