e190b65474ff8ff0f15115c51f658d7f486c84ca
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2018-04-03 21:45:49 +0800

    complete the RESTful API for the class of wordladder

--- src/main/java/com/example/demo/CheckIn.java
+++ src/main/java/com/example/demo/CheckIn.java
@@ -1,11 +1,29 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.FileNotFoundException;
 import java.util.Vector;
 
 @RestController
 public class CheckIn {
-    private Vector<String> Ladder;
+    private Vector<String> ans;
+    private boolean flag = false;
+    private WordLadder ladder;
 
+    @RequestMapping("/")
+    public Vector<String> getLadder (@RequestParam(value = "begin", defaultValue = "") String begin,
+                      @RequestParam(value = "end", defaultValue = "") String end){
+        if (!flag){
+            try {
+                ladder.readFile();
+            }
+            catch (FileNotFoundException e) {
+            }
+        }
+        ans = ladder.findLadder(begin,end);
+        return ans;
+    }
 }
